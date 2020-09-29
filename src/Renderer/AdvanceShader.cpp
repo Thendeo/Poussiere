91e7964a6f731f6e49ca0ce7b4712f94db5fb9c8
Thendeo <alafont@protonmail.com>
2020-09-29 18:27:21 +0200

    Updated AdvanceShader skeleton from new design

--- src/Renderer/AdvanceShader.cpp
+++ src/Renderer/AdvanceShader.cpp
@@ -1,7 +1,28 @@
+/*----------------------------------------------------
+**
+**		Date: 2020
+**		Author: Thendeo
+**		Project: Particules
+**		File: AdvanceShader implementation file
+**
+----------------------------------------------------*/
+
 #include "AdvanceShader.h"
+#include "AssertHdl.h"
 
 AdvanceShader::AdvanceShader()
+: Shader("AdvanceParticules.vertexshader", "AdvanceParticules.geometryshader", "AdvanceParticules.fragmentshader")
+, m_UniformTexturePosition(0U)
+, m_UniformTextureVelocity(0U)
 {
+	glUseProgram(m_ProgramID);
+
+	m_UniformTexturePosition = glGetUniformLocation(m_ProgramID, "particulePosition");
+	m_UniformTextureVelocity = glGetUniformLocation(m_ProgramID, "particuleVelocity");
+
+
+	GLenum l_Err = glGetError();
+	doAssert(l_Err == GL_NO_ERROR);
 }
 
 AdvanceShader::~AdvanceShader()
@@ -10,12 +31,15 @@ AdvanceShader::~AdvanceShader()
 
 void AdvanceShader::draw()
 {
+	doAssert(false);
 }
 
-void AdvanceShader::setVertexParameters()
+void AdvanceShader::setFragmentPosition(GLuint p_TexturePosition)
 {
+	doAssert(false);
 }
 
-void AdvanceShader::loadTexture()
+void AdvanceShader::setFragmentVelocity(GLuint p_TextureVelocity)
 {
+	doAssert(false);
 }
--- src/Renderer/AdvanceShader.h
+++ src/Renderer/AdvanceShader.h
@@ -7,16 +7,29 @@
 **
 ----------------------------------------------------*/
 
-#pragma once
+/*----------------------------------------------------
+**
+**		Date: 2020
+**		Author: Thendeo
+**		Project: Particules
+**		File: AdvanceShader header file
+**
+----------------------------------------------------*/
 
+#pragma once
 #include "Shader.h"
 
 #include <GL/glew.h>
 
-//! @brief Shader to dvance a particule
-//! Enable one to combine two textures by adding them
-//! Input for the vertex shader : point with GS or member triangle ?
-//! Pixel shader render adds two texture to a render buffer
+//! @brief Shader to draw particules
+//! Allows one to update position texture given a velocity map
+//! Vertex shader input : 
+//! * None
+//! Geometry shader input :
+//! * None
+//! Fragment shader input :
+//! * Position texture
+//! * Velocity Map
 class AdvanceShader : public Shader
 {
 public:
@@ -26,19 +39,12 @@ public:
 
 	virtual void draw();
 
-	void setVertexParameters(/*Triangle or point ?*/); // useless ?
-	void loadTexture(/*buffer1 + buffer2*/);
+	void setFragmentPosition(GLuint p_TexturePosition);
+	void setFragmentVelocity(GLuint p_TextureVelocity);
 
 private:
 
-	// Arrays
-	GLuint m_VertexArrayID;
-	GLuint m_TextureID;
-
-	// BufferData
-	GLuint m_Vertexbuffer;
-
-	GLuint m_ProgramID;
-
-	bool m_TextureLoaded;
+	// Uniform location
+	GLuint m_UniformTexturePosition;
+	GLuint m_UniformTextureVelocity;
 };
